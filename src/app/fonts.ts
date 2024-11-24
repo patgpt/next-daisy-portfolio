ace308d0e3d80592a0a0e8c86fa3de6c679b3c20
patgpt <doitformurphy@gmail.com>
2024-11-24 17:47:34 -0400

    Add loading, hero, header components and configure Tailwind CSS with daisyUI

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+// fonts.ts;
+import localFont from "next/font/local";
+
+const fontSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const fontMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export { fontSans, fontMono };
---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+
+export const Loading = () => {
+  return (
+    <div>
+      <span className="loading loading-infinity loading-lg"></span>
+    </div>
+  )
+}
+Loading.displayName = 'Loading';
+export default Loading;
